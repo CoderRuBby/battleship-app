7f507f90ed88d877aa48b70ba8fec11dfe7cc0ff
Ruben Jacobo <123346491+CoderRuBby@users.noreply.github.com>
2025-05-16 17:04:43 -0700

    feat: Initialize player/opponent ships.

---
+++ app/utils/ship-object.ts
@@ -0,0 +1,25 @@
+export default class Ship {
+    name: string;
+    length: number;
+    hit: number;
+    sunk: boolean;
+
+    constructor(name: string, length: number) {
+        this.name = name;
+        this.length = length;
+        this.hit = 0;
+        this.sunk = false;
+    }
+
+    isHit() {
+        this.hit++;
+        this.isSunk();
+    }
+
+    isSunk() {
+        if (this.hit === this.length) {
+            this.sunk = true;
+        }
+    }
+
+}
