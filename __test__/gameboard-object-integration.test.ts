e9d66a6c2c5b3bc70a2084516b8a3549f4a15aad
Ruben Jacobo <123346491+CoderRuBby@users.noreply.github.com>
2025-05-24 16:10:36 -0700

    test: Ship buttons select/deselect a ship.

--- __test__/gameboard-object-integration.test.ts
+++ __test__/gameboard-object-integration.test.ts
@@ -1,8 +1,14 @@
+import { beforeEach } from 'vitest';
 import { describe, it, expect } from 'vitest';
 import GameBoard from '~/utils/gameboard-object';
+import Ship from '~/utils/ship-object';
 
 describe('GameBoard', () => {
-  const testGameBoard = new GameBoard();
+  let testGameBoard: GameBoard;
+
+  beforeEach(() => {
+    testGameBoard = new GameBoard();
+  });
 
   describe('getShipPaths', () => {
     it('can output all ship paths', () => {
@@ -12,6 +18,32 @@ describe('GameBoard', () => {
         [0, 1, 2],
         [0, 10, 20],
       ]);
+    });
+  });
+  describe('shipOnClick', () => {
+    const TestShip = new Ship('foo', 3);
+    const SecondTestShip = new Ship('bar', 5);
+
+    it('can select a ship', () => {
+      testGameBoard.shipOnClick(TestShip);
+
+      expect(testGameBoard.selectedShip).toEqual(TestShip);
+    });
+
+    it('can deselect the ship if clicked again', () => {
+      testGameBoard.shipOnClick(TestShip);
+      expect(testGameBoard.selectedShip).toEqual(TestShip);
+
+      testGameBoard.shipOnClick(TestShip);
+
+      expect(testGameBoard.selectedShip).toEqual('none');
+    });
+
+    it('can select a different ship', () => {
+      testGameBoard.shipOnClick(TestShip);
+      testGameBoard.shipOnClick(SecondTestShip);
+
+      expect(testGameBoard.selectedShip).toEqual(SecondTestShip);
     });
   });
 });
