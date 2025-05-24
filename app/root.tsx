8d05e457f9f693347888e1250b556d930283c03e
Ruben Jacobo <123346491+CoderRuBby@users.noreply.github.com>
2025-05-24 16:15:45 -0700

    ref: Format document to prettier standards.

--- app/root.tsx
+++ app/root.tsx
@@ -1,65 +1,54 @@
 import {
-    Outlet,
-    Scripts,
-    ScrollRestoration,
-    isRouteErrorResponse,
-  } from "react-router";
-  import type { Route } from "./+types/root";
-  
-  import appStylesHref from "./app.css?url";
-  
-  export default function Layout() {
-    return (
-      <html lang="en">
-        <head>
-          <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1"
-          />
-          <link rel="stylesheet" href={appStylesHref} />
-        </head>
-        <body>
-          <Outlet />
-          <ScrollRestoration />
-          <Scripts />
-        </body>
-      </html>
-    );
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  isRouteErrorResponse,
+} from 'react-router';
+import type { Route } from './+types/root';
+import appStylesHref from './app.css?url';
+
+export default function Layout() {
+  return (
+    <html lang='en'>
+      <head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='stylesheet' href={appStylesHref} />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  let message = 'Oops!';
+  let details = 'An unexpected error occurred.';
+  let stack: string | undefined;
+
+  if (isRouteErrorResponse(error)) {
+    message = error.status === 404 ? '404' : 'Error';
+    details =
+      error.status === 404
+        ? 'The requested page could not be found.'
+        : error.statusText || details;
+  } else if (import.meta.env.DEV && error && error instanceof Error) {
+    details = error.message;
+    stack = error.stack;
   }
-  
-  export function ErrorBoundary({
-    error,
-  }: Route.ErrorBoundaryProps) {
-    let message = "Oops!";
-    let details = "An unexpected error occurred.";
-    let stack: string | undefined;
-  
-    if (isRouteErrorResponse(error)) {
-      message = error.status === 404 ? "404" : "Error";
-      details =
-        error.status === 404
-          ? "The requested page could not be found."
-          : error.statusText || details;
-    } else if (
-      import.meta.env.DEV &&
-      error &&
-      error instanceof Error
-    ) {
-      details = error.message;
-      stack = error.stack;
-    }
-  
-    return (
-      <main id="error-page">
-        <h1>{message}</h1>
-        <p>{details}</p>
-        {stack && (
-          <pre>
-            <code>{stack}</code>
-          </pre>
-        )}
-      </main>
-    );
-  }
-  
+
+  return (
+    <main id='error-page'>
+      <h1>{message}</h1>
+      <p>{details}</p>
+      {stack && (
+        <pre>
+          <code>{stack}</code>
+        </pre>
+      )}
+    </main>
+  );
+}
