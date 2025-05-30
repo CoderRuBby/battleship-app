98af0b879d529686fceda6268863ee21f1cd89f4
Ruben Jacobo <123346491+CoderRuBby@users.noreply.github.com>
2025-05-30 14:43:18 -0700

    test: Ai gameboard can assign a ship a startPoint.

--- __test__/ai-gameboard.test.ts
+++ __test__/ai-gameboard.test.ts
@@ -6,6 +6,16 @@ import Ship from '~/utils/ship-object';
 describe('AiGameBoard', () => {
   const Ai = new AiGameBoard();
   const Ship1 = new Ship('foo', 2);
+  const Ship2 = new Ship('bar', 3);
+  const allShips = [Ship1, Ship2];
+
+  describe('getShipStartPoint', () => {
+    it('can assign shipStart', () => {
+      Ai.getShipStartPoint(Ship1, 4);
+
+      expect(Ship1.shipStartPoint).toBe(4);
+    });
+  });
 
   describe('placeShip', () => {
     it('can place a single ship on the gameboard', () => {
