de64a41cc7e5251993da8e22cedec5750fe48df9
Ruben Jacobo <123346491+CoderRuBby@users.noreply.github.com>
2025-05-30 14:44:42 -0700

    feat: Ai gameboard can assign a ship a startPoint.

--- app/utils/ai-gameboard.ts
+++ app/utils/ai-gameboard.ts
@@ -2,12 +2,25 @@ import GameBoard from './gameboard-object';
 import Ship from './ship-object';
 
 class AiGameBoard extends GameBoard {
+  availableSquares: Set<number>;
   constructor() {
     super();
+    this.availableSquares = new Set();
+    this.initializeAvailableSquares();
   }
 
-  placeShip(ship: Ship, shipStartPoint: number, shipEndPoint: number) {
-    this.placeShipOnGameBoard(ship, shipStartPoint, shipEndPoint);
+  initializeAvailableSquares() {
+    for (let i = 0; i < 100; i++) {
+      this.availableSquares.add(i);
+    }
+  }
+
+  getShipStartPoint(ship: Ship, number: number) {
+    const availableSquaresArray = [...this.availableSquares];
+    const startPoint = availableSquaresArray[number];
+    this.assignShipStartPoint(ship, startPoint);
+  }
+
   }
 }
 
