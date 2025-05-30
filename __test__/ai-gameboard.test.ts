bdb8e62410a3476b44e425f53607037815cd6577
Ruben Jacobo <123346491+CoderRuBby@users.noreply.github.com>
2025-05-30 16:52:10 -0700

    test[ref]: test[ref]: Initialize setup test objects.

--- __test__/ai-gameboard.test.ts
+++ __test__/ai-gameboard.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 
 import AiGameBoard from '~/utils/ai-gameboard';
 import Ship from '~/utils/ship-object';
