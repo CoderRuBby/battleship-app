96b870109d8ef3f1e9e08fdd00ebf8028fb1ffa7
Ruben Jacobo <123346491+CoderRuBby@users.noreply.github.com>
2025-06-29 16:10:54 -0700

    test: Implement `AiAttack` class and `randomAttackLocation` method.
    - Created the `AiAttack` class with a `randomAttackLocation` method to determine a random attack coordinate.
    - Added a test suite for the `AiAttack` class, specifically testing the `randomAttackLocation` method to ensure it returns a number.

---
+++ __test__/AiAttack.test.ts
@@ -0,0 +1,21 @@
+import { describe, it, beforeEach, expect } from 'vitest';
+import AiAttack from '~/utils/AiAttack';
+import GameBoard from '~/utils/GameBoard';
+
+describe('AiAttack', () => {
+  let Ai: AiAttack;
+  let Player: GameBoard;
+
+  beforeEach(() => {
+    Player = new GameBoard();
+    Ai = new AiAttack();
+  });
+
+  describe('randomAttackLocation', () => {
+    it('will return a number', () => {
+      const randomLocation = Ai.randomAttackLocation(Player);
+
+      expect(Number.isInteger(randomLocation)).toBe(true);
+    });
+  });
+});
