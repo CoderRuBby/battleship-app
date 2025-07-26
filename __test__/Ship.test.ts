f991a1d4813f215bd303f38674d2b4b1e32dca2c
Ruben Jacobo <123346491+CoderRuBby@users.noreply.github.com>
2025-07-25 19:58:23 -0700

    test: Add unit tests for Ship class methods.
    -Add test case that verifies `isHit` method adds hit location to `hitLocations` property.
    -Add test case that verifies `getHitLocations` method will return all hit locations on the ship.

---
+++ __test__/Ship.test.ts
@@ -0,0 +1,36 @@
+import { describe, beforeEach, it, expect } from 'vitest';
+import GameBoard from '~/utils/GameBoard';
+import Ship from '~/utils/Ship';
+
+describe('Ship', () => {
+  let Ship1: Ship;
+  let NewGameBoard: GameBoard;
+
+  beforeEach(() => {
+    Ship1 = new Ship('Ship1', 4);
+    NewGameBoard = new GameBoard();
+  });
+
+  describe('isHit', () => {
+    it('will add hit location to hitLocations property', () => {
+      const hitLocation = 45;
+
+      Ship1.isHit(hitLocation);
+
+      expect(Ship1.hitLocations).toContain(hitLocation);
+    });
+  });
+
+  describe('getHitLocations', () => {
+    it('can return all locations that are hit on Ship1', () => {
+      const firstHit = 5;
+      const secondHit = 6;
+
+      Ship1.isHit(firstHit);
+      Ship1.isHit(secondHit);
+      Ship1.getHitLocations();
+
+      expect(Ship1.hitLocations).toEqual([5, 6]);
+    });
+  });
+});
