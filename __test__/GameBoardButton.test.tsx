23c8f2d92f37aa9b83c666e60802e0df42d22562
Ruben Jacobo <123346491+CoderRuBby@users.noreply.github.com>
2025-09-27 13:39:50 -0700

    test: Add GameBoardButton test suite.
    - Add test case to verify a default button will render.

---
+++ __test__/GameBoardButton.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import { GameBoardButton } from '~/components/GameBoardButton';
+
+describe('GameBoardButton', () => {
+  it('will render a default button', () => {
+    render(<GameBoardButton testId='button' />);
+
+    const button = screen.getByTestId('button');
+    const buttonStyle = getComputedStyle(button);
+
+    expect(buttonStyle.background).toContain('rgba(0, 0, 0, 0)');
+  });
+});
