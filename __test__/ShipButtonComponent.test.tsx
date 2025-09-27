964e5515f54ee7787ddd6ca2d3fd86d088547a80
Ruben Jacobo <123346491+CoderRuBby@users.noreply.github.com>
2025-09-27 12:27:55 -0700

    test: Verify only one button can be selected in ShipButtonComponent.

--- __test__/ShipButtonComponent.test.tsx
+++ __test__/ShipButtonComponent.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import { beforeEach, describe, expect, it } from 'vitest';
+import userEvent from '@testing-library/user-event';
+import { beforeEach, describe, expect, it, test } from 'vitest';
 import { ShipButtonComponent } from '~/components/ShipButtonComponent';
 
 describe('ShipButtonComponent', () => {
@@ -31,5 +32,37 @@ describe('ShipButtonComponent', () => {
     expect(buttonOne).toBeInTheDocument();
     expect(buttonTwo).toBeInTheDocument();
     expect(buttonThree).toBeInTheDocument();
+  });
+
+  test('only one button can be selected at a time', async () => {
+    const user = userEvent.setup();
+
+    render(component);
+
+    const buttonOne = screen.getByTestId('button1');
+    const styleOne = getComputedStyle(buttonOne);
+
+    expect(styleOne.background).toContain('url("button1.png")');
+
+    await user.click(buttonOne);
+
+    const newStyleOne = getComputedStyle(buttonOne);
+
+    expect(newStyleOne.background).toContain('url("highlightedbutton1.png")');
+
+    const buttonTwo = screen.getByTestId('button2');
+    const styleTwo = getComputedStyle(buttonTwo);
+
+    expect(styleTwo.background).toContain('url("button2.png")');
+
+    await user.click(buttonTwo);
+
+    const newStyleTwo = getComputedStyle(buttonTwo);
+    const buttonOneAgain = screen.getByTestId('button1');
+    const styleOneAgain = getComputedStyle(buttonOneAgain);
+
+    screen.debug();
+    expect(newStyleTwo.background).toContain('url("highlightedbutton2.png")');
+    expect(styleOneAgain.background).toContain('url("button1.png")');
   });
 });
