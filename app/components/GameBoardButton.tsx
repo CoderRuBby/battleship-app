7fd107bf418c92504760ead06991b0a8410a839c
Ruben Jacobo <123346491+CoderRuBby@users.noreply.github.com>
2025-09-27 14:51:51 -0700

    feat: Add isHit conditional to background image selection to render hit image.

--- app/components/GameBoardButton.tsx
+++ app/components/GameBoardButton.tsx
@@ -2,18 +2,22 @@ interface GameBoardButtonProps {
   testId: string;
   selectedShip: string | null;
   shipImageNumber: string | null;
+  isHit: boolean | null;
 }
 
 export function GameBoardButton({
   testId,
   selectedShip,
   shipImageNumber,
+  isHit,
 }: GameBoardButtonProps) {
   const buttonStyle = {
     background:
       selectedShip === testId
         ? `url("${selectedShip}${shipImageNumber}.png")`
-        : 'rgba(0, 0, 0, 0)',
+        : isHit === true
+          ? 'url("hit.png")'
+          : 'rgba(0, 0, 0, 0)',
   };
   return <button data-testid={testId} style={buttonStyle}></button>;
 }
