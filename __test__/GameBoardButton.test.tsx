e0abd276b0aced91850457bb1effdee920615d63
Ruben Jacobo <123346491+CoderRuBby@users.noreply.github.com>
2025-09-27 14:06:41 -0700

    test: Add test case that verifies onhover changes button background to a ship.

--- __test__/GameBoardButton.test.tsx
+++ __test__/GameBoardButton.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { describe, expect, it } from 'vitest';
 import { GameBoardButton } from '~/components/GameBoardButton';
 
@@ -10,5 +11,30 @@ describe('GameBoardButton', () => {
     const buttonStyle = getComputedStyle(button);
 
     expect(buttonStyle.background).toContain('rgba(0, 0, 0, 0)');
+  });
+
+  describe('during ship selection phase', () => {
+    it('will have a ship background image on hover', async () => {
+      const user = userEvent.setup();
+
+      render(
+        <GameBoardButton
+          testId='button'
+          selectedShip='button'
+          shipImageNumber='1'
+        />,
+      );
+
+      const button = screen.getByTestId('button');
+      let style = getComputedStyle(button);
+
+      expect(style.background).toContain('rgba(0, 0, 0, 0)');
+
+      await user.hover(button);
+
+      style = getComputedStyle(button);
+
+      expect(style.background).toContain('url("button1.png")');
+    });
   });
 });
