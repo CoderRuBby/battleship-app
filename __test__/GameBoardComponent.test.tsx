7ecfd5fad8e8dc3f91fb200aad2321a84918834e
Ruben Jacobo <123346491+CoderRuBby@users.noreply.github.com>
2025-10-17 19:28:14 -0700

    test: Verify `mouseenter` will not render more possible locations when a square is selected.

--- __test__/GameBoardComponent.test.tsx
+++ __test__/GameBoardComponent.test.tsx
@@ -175,4 +175,75 @@ describe('GameBoardComponent', () => {
     const button25Style = getComputedStyle(button25);
     expect(button25Style.background).toContain('url("foo0up.png")');
   });
+
+  test('mouseenter will not render more possible locations when a square is selected', async () => {
+    const user = userEvent.setup();
+
+    render(componentWithSelectedShip);
+
+    const button45 = screen.getByTestId('45');
+    const button35 = screen.getByTestId('35');
+
+    await user.hover(button45);
+    await user.click(button45);
+    shipObject.addShipStart(45);
+
+    await user.hover(button35);
+
+    const button36 = screen.getByTestId('36');
+    const button36Style = getComputedStyle(button36);
+    expect(button36Style.background).not.toContain('url("foo1right.png")');
+    expect(button36Style.background).toContain('rgba(0, 0, 0, 0)');
+
+    const button37 = screen.getByTestId('37');
+    const button37Style = getComputedStyle(button37);
+    expect(button36Style.background).not.toContain('url("foo2right.png")');
+    expect(button37Style.background).toContain('rgba(0, 0, 0, 0)');
+
+    const button34 = screen.getByTestId('34');
+    const button34Style = getComputedStyle(button34);
+    expect(button36Style.background).not.toContain('url("foo1left.png")');
+    expect(button34Style.background).toContain('rgba(0, 0, 0, 0)');
+
+    const button33 = screen.getByTestId('33');
+    const button33Style = getComputedStyle(button33);
+    expect(button36Style.background).not.toContain('url("foo0left.png")');
+    expect(button33Style.background).toContain('rgba(0, 0, 0, 0)');
+
+    const button45Style = getComputedStyle(button45);
+    expect(button45Style.background).not.toContain('url("foo2up.png")');
+    expect(button45Style.background).not.toContain('url("foo2left.png")');
+    expect(button45Style.background).toContain('rgba(0, 0, 0, 0)');
+
+    const button46 = screen.getByTestId('46');
+    const button46Style = getComputedStyle(button46);
+    expect(button46Style.background).toContain('url("foo1right.png")');
+
+    const button47 = screen.getByTestId('47');
+    const button47Style = getComputedStyle(button47);
+    expect(button47Style.background).toContain('url("foo2right.png")');
+
+    const button55 = screen.getByTestId('55');
+    const button55Style = getComputedStyle(button55);
+    expect(button55Style.background).toContain('url("foo1down.png")');
+
+    const button65 = screen.getByTestId('65');
+    const button65Style = getComputedStyle(button65);
+    expect(button65Style.background).toContain('url("foo2down.png")');
+
+    const button44 = screen.getByTestId('44');
+    const button44Style = getComputedStyle(button44);
+    expect(button44Style.background).toContain('url("foo1left.png")');
+
+    const button43 = screen.getByTestId('43');
+    const button43Style = getComputedStyle(button43);
+    expect(button43Style.background).toContain('url("foo0left.png")');
+
+    const button35Style = getComputedStyle(button35);
+    expect(button35Style.background).toContain('url("foo1up.png")');
+
+    const button25 = screen.getByTestId('25');
+    const button25Style = getComputedStyle(button25);
+    expect(button25Style.background).toContain('url("foo0up.png")');
+  });
 });
