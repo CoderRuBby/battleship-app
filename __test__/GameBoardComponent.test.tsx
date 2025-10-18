2ea365d6bbed94883abe84aa319cb13146650b8d
Ruben Jacobo <123346491+CoderRuBby@users.noreply.github.com>
2025-10-17 17:05:51 -0700

    test: Verify mouseenter renders the selected ship image on possible locations.

--- __test__/GameBoardComponent.test.tsx
+++ __test__/GameBoardComponent.test.tsx
@@ -2,29 +2,21 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { beforeEach, describe, expect, it, test } from 'vitest';
 import { GameBoardComponent } from '~/components/GameBoardComponent';
-import { gameBoard } from '~/utils/GameBoard';
-import type { gameBoardInterface } from '~/utils/GameBoard';
 import { ship } from '~/utils/Ship';
 import type { shipInterface } from '~/utils/Ship';
 
 describe('GameBoardComponent', () => {
   let shipObject: shipInterface;
-  let gameBoardObject: gameBoardInterface;
   let component: React.ReactElement;
   let componentWithSelectedShip: React.ReactElement;
 
   beforeEach(() => {
     shipObject = ship('foo', 3);
-    shipObject = ship('foo', 3);
-    gameBoardObject = { board: gameBoard().board };
     component = (
-      <GameBoardComponent gameBoard={gameBoardObject} selectedShip={null} />
+      <GameBoardComponent allShips={[shipObject]} selectedShip={null} />
     );
     componentWithSelectedShip = (
-      <GameBoardComponent
-        gameBoard={gameBoardObject}
-        selectedShip={shipObject}
-      />
+      <GameBoardComponent allShips={[shipObject]} selectedShip={shipObject} />
     );
   });
   it('can render a container with 100 GameBoardButtons', () => {
@@ -41,8 +33,9 @@ describe('GameBoardComponent', () => {
 
     const button45 = screen.getByTestId('45');
     await user.hover(button45);
-
     const button45Style = getComputedStyle(button45);
+    expect(button45Style.background).not.toContain('url("foo2up.png")');
+    expect(button45Style.background).not.toContain('url("foo2left.png")');
     expect(button45Style.background).toContain('rgba(0, 0, 0, 0)');
 
     const button46 = screen.getByTestId('46');
@@ -67,7 +60,7 @@ describe('GameBoardComponent', () => {
 
     const button43 = screen.getByTestId('43');
     const button43Style = getComputedStyle(button43);
-    expect(button43Style.background).toContain('url("foo2left.png")');
+    expect(button43Style.background).toContain('url("foo0left.png")');
 
     const button35 = screen.getByTestId('35');
     const button35Style = getComputedStyle(button35);
@@ -75,6 +68,6 @@ describe('GameBoardComponent', () => {
 
     const button25 = screen.getByTestId('25');
     const button25Style = getComputedStyle(button25);
-    expect(button25Style.background).toContain('url("foo2up.png")');
+    expect(button25Style.background).toContain('url("foo0up.png")');
   });
 });
