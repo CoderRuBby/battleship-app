0f5f9005204ce299b88889e0b1caccff6c28b24d
Ruben Jacobo <123346491+CoderRuBby@users.noreply.github.com>
2025-12-29 19:57:40 -0800

    ref: ref: Refactor AiPlaceShips.ts into a function that returns an object to align with functional programming.
    - Rename `AiPlaceShips` to `aiShipPlacement`.
    - Add `aiShipPlacementSystemInterface`.
    - Update associated test files to accept new changes.

--- __test__/AiPlaceShips.test.ts
+++ __test__/AiPlaceShips.test.ts
@@ -1,22 +1,38 @@
 import { describe, it, beforeEach, expect } from 'vitest';
-import AiPlaceShips from '~/utils/AiPlaceShips';
-import AiGameBoard from '~/utils/AiGameBoard';
+import type { aiShipPlacementSystemInterface } from '~/utils/aiShipPlacementSystem';
+import aiShipPlacementSystem from '~/utils/aiShipPlacementSystem';
+import type { gameBoardInterface } from '~/utils/GameBoard';
+import gameBoard from '~/utils/GameBoard';
+import type { shipInterface } from '~/utils/Ship';
+import ship from '~/utils/Ship';
 
 describe('AiPlaceShips', () => {
-  let Ai: AiGameBoard;
-  let AiPlayerPlaceShips: AiPlaceShips;
+  let carrier: shipInterface;
+  let destroyer: shipInterface;
+  let submarine: shipInterface;
+  let battleship: shipInterface;
+  let cruiser: shipInterface;
+  let shipsArray: shipInterface[];
+  let Ai: gameBoardInterface;
+  let AiPlayerPlaceShips: aiShipPlacementSystemInterface;
 
   beforeEach(() => {
-    Ai = new AiGameBoard();
-    AiPlayerPlaceShips = new AiPlaceShips(Ai);
+    carrier = ship('carrier', 5);
+    destroyer = ship('destroyer', 3);
+    submarine = ship('submarine', 3);
+    battleship = ship('battleship', 4);
+    cruiser = ship('cruiser', 2);
+    shipsArray = [carrier, destroyer, submarine, battleship, cruiser];
+    Ai = gameBoard(shipsArray);
+    AiPlayerPlaceShips = aiShipPlacementSystem(Ai);
   });
 
   describe('randomSquare', () => {
     it('will not return a square that has a ship placed', () => {
       const testSquare = 45;
 
-      Ai.gameboard[testSquare].ship = Ai.Battleship;
-      Ai.selectedShip = Ai.Battleship;
+      Ai.board[testSquare].ship = Ai.allShips[0];
+      Ai.selectedShip = Ai.allShips[0];
 
       const randomSquare = AiPlayerPlaceShips.randomSquare(100, testSquare);
 
@@ -29,16 +45,16 @@ describe('AiPlaceShips', () => {
       AiPlayerPlaceShips.placeShipOnGameBoard();
 
       expect(Ai.allShipsPlaced).toBe(true);
-      expect(Number.isInteger(Ai.Carrier.shipStartPoint)).toBe(true);
-      expect(Number.isInteger(Ai.Carrier.shipEndPoint)).toBe(true);
-      expect(Number.isInteger(Ai.Battleship.shipStartPoint)).toBe(true);
-      expect(Number.isInteger(Ai.Battleship.shipEndPoint)).toBe(true);
-      expect(Number.isInteger(Ai.Destroyer.shipStartPoint)).toBe(true);
-      expect(Number.isInteger(Ai.Destroyer.shipEndPoint)).toBe(true);
-      expect(Number.isInteger(Ai.Submarine.shipStartPoint)).toBe(true);
-      expect(Number.isInteger(Ai.Submarine.shipEndPoint)).toBe(true);
-      expect(Number.isInteger(Ai.Cruiser.shipStartPoint)).toBe(true);
-      expect(Number.isInteger(Ai.Cruiser.shipEndPoint)).toBe(true);
+      expect(Number.isInteger(Ai.allShips[0].shipStartPoint)).toBe(true);
+      expect(Number.isInteger(Ai.allShips[0].shipEndPoint)).toBe(true);
+      expect(Number.isInteger(Ai.allShips[1].shipStartPoint)).toBe(true);
+      expect(Number.isInteger(Ai.allShips[1].shipEndPoint)).toBe(true);
+      expect(Number.isInteger(Ai.allShips[2].shipStartPoint)).toBe(true);
+      expect(Number.isInteger(Ai.allShips[2].shipEndPoint)).toBe(true);
+      expect(Number.isInteger(Ai.allShips[3].shipStartPoint)).toBe(true);
+      expect(Number.isInteger(Ai.allShips[3].shipEndPoint)).toBe(true);
+      expect(Number.isInteger(Ai.allShips[4].shipStartPoint)).toBe(true);
+      expect(Number.isInteger(Ai.allShips[4].shipEndPoint)).toBe(true);
     });
   });
 });
