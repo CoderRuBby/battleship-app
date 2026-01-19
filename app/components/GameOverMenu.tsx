78ad538b0ca0cca86b5de927f033fe8646bba827
Ruben Jacobo <123346491+CoderRuBby@users.noreply.github.com>
2026-01-19 08:13:48 -0800

    feat: Game over menu renders a play again button.

--- app/components/GameOverMenu.tsx
+++ app/components/GameOverMenu.tsx
@@ -6,6 +6,7 @@ export function GameOverMenu({ winLoseText }: gameOverMenuInterface) {
   return (
     <div role='dialog'>
       <h1>You {winLoseText}</h1>
+      <button>Play Again</button>
     </div>
   );
 }
