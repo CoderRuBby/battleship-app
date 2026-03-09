47733725b272ec6ad98de9e23f7af733c25837c1
Ruben Jacobo <123346491+CoderRuBby@users.noreply.github.com>
2026-03-08 21:04:42 -0700

    feat: Ship button will not be able to be selected after ship is placed on the gameboard.

--- app/components/ShipButton.tsx
+++ app/components/ShipButton.tsx
@@ -22,12 +22,21 @@ export function ShipButton({
           ? `url("outline-${testId}.png")`
           : `url("${testId}.png")`,
   };
+
+  const isDisabled = () => {
+    if (playerOne.props.allShips[shipIndex].props.isPlaced) {
+      return true;
+    } else {
+      return false;
+    }
+  };
   return (
     <button
       className='ship-button'
       data-testid={testId}
       style={buttonStyle}
       onClick={shipOnClick}
+      disabled={isDisabled()}
     ></button>
   );
 }
