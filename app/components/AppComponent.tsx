1827cbc674cb4df5a930036408e06a897f0dd3df
Ruben Jacobo <123346491+CoderRuBby@users.noreply.github.com>
2026-07-03 17:22:07 -0700

    style: Improve game board design.

--- app/components/AppComponent.tsx
+++ app/components/AppComponent.tsx
@@ -273,8 +273,7 @@ export function AppComponent({
           className="
           flex flex-col-reverse justify-center items-center gap-6
           bg-[url('/images/ship-container.svg')]
-          shadow-[0px_0px_3px_5px_rgba(44,943,444,.1),0px_-5px_4px_10px_rgba(23,94,844,.3),0px_-6px_20px_33px_rgba(3,78,933,.3),0px_-3px_6px_18px_rgba(33,334,943,.75)]
-          h-auto w-fit bg-[10%] p-1 mb-30 backdrop-blur-[4px]
+           shadow-[0px_0px_8px_0px_rgba(44,255,255,.1),0px_19px_9px_1px_rgba(23,94,210,.3),0px_-3px_5px_3px_rgba(3,78,255,.3),0px_1px_7px_2px_rgba(33,255,255,.75)]
           md:h-auto md:w-fit md:bg-[17%] md:flex-row md:p-5 md:pb-15 md:pt-20 md:m-0 md:gap-[clamp(1rem,25vw,4rem)]
         "
         >
