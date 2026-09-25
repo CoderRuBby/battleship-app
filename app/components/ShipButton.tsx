556cad4c6d841cf9925fe10d98a89e6d6101e147
Ruben Jacobo <123346491+CoderRuBby@users.noreply.github.com>
2026-09-25 13:34:40 -0700

    style: Add layout helpers to scale the ship buttons.
    - Remove old css.

--- app/components/ShipButton.tsx
+++ app/components/ShipButton.tsx
@@ -26,12 +26,29 @@ export function ShipButton({ testId, shipOnClick, player }: ShipButtonProps) {
       return false;
     }
   };
+
+  const shipDiv = () => {
+    const divClass =
+      'w-[1.75rem] h-[1.75rem] pointer-coarse:portrait:xs:w-[2.25rem] [@media(max-height:700px)]:h-7 [@media(max-height:700px)]:w-7 pointer-coarse:portrait:xs:h-[2.25rem] portrait:sm:w-[2rem] portrait:sm:h-[2rem] pointer-coarse:portrait:md:w-[2.625rem] pointer-coarse:portrait:md:h-[2.625rem] pointer-coarse:portrait:lg:w-[3.5rem] pointer-coarse:portrait:lg:h-[3.5rem] portrait:md:w-8.5 portrait:md:h-8.5 landscape:md:w-9.5 landscape:md:h-9.5 landscape:lg:w-11.5 landscape:lg:h-11.5 landscape:xl:w-15 landscape:xl:h-15';
+
+    const shipIndex = player.props.allShips.findIndex(
+      (i) => i.props.name === testId,
+    );
+    const ship = player.props.allShips[shipIndex];
+    const array: number[] = Array.from({ length: ship.props.length });
+    return array.map((arr) => {
+      return <div key={arr} className={divClass} />;
+    });
+  };
+
   return (
     <button
-      className={`ship-button ${testId} ${isSelected()} h-[clamp(1.7rem,2vw,2.5rem)] xl:h-10`}
+      className={`ship-button ${testId} ${isSelected()} flex row`}
       data-testid={testId}
       onClick={shipOnClick}
       disabled={isDisabled()}
-    ></button>
+    >
+      <>{shipDiv()}</>
+    </button>
   );
 }
